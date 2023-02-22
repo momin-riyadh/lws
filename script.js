e03bea9e0ed2589c47ed2bbf9cff399d6de931ae
unknown <mominriyadh@gmail.com>
2023-02-22 23:54:38 +0600

    fix and deploy

--- script.js
+++ script.js
@@ -3,6 +3,25 @@ const counterEL = document.getElementById('counter');
 const incrementEL = document.getElementById('increment');
 const decrementEL = document.getElementById('decrement');
 
+//Action Creators
+const increment = () => {
+    return {
+        type: 'INCREMENT',
+        payload: 5
+    }
+}
+
+const decrement = () => {
+    return {
+        type: 'DECREMENT',
+        payload: 2
+    }
+}
+
+// Action Identifiers
+const INCREMENT = 'increment';
+const DECREMENT = 'decrement';
+
 
 //Initial State
 const initialState = {
@@ -11,15 +30,15 @@ const initialState = {
 
 // Create Reducer Function
 function counterReducer(state = initialState, action) {
-    if (action.type === 'increment') {
+    if (action.type === 'INCREMENT') {
         return {
             ...state,
-            value: state.value + 1,
+            value: state.value + action.payload,
         }
-    } else if (action.type === 'decrement') {
+    } else if (action.type === 'DECREMENT') {
         return {
             ...state,
-            value: state.value - 1,
+            value: state.value - action.payload,
         }
     } else {
         return state;
@@ -40,13 +59,9 @@ store.subscribe(render);
 
 //Event Lisntener
 incrementEL.addEventListener('click', () => {
-    store.dispatch({
-        type: 'increment',
-    })
+    store.dispatch(increment())
 })
 
 decrementEL.addEventListener('click', () => {
-    store.dispatch({
-        type: 'decrement',
-    })
+    store.dispatch(decrement())
 })
