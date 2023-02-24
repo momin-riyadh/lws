a586a64ef41b82dd34c887b6755433f5eb6004a9
unknown <mominriyadh@gmail.com>
2023-02-24 21:33:57 +0600

    fix and deploy

--- src/App.js
+++ src/App.js
@@ -7,7 +7,13 @@ export default function App() {
     return (
         <Provider store={store}>
             <div className="w-screen h-screen p-10 bg-gray-100 text-slate-700">
+                <h1 className="max-w-md mx-auto text-center text-2xl font-bold">
+                    Simple Counter Application
+                </h1>
 
+                <div className="max-w-md mx-auto mt-10 space-y-5">
+                    <Counter id="1"/>
+                </div>
             </div>
         </Provider>
     );
