784a63bf34ab5d4581f8e72a734b6ca4d40d0a36
momin-riyadh <mominriyadh@gmail.com>
2023-03-25 16:07:42 +0600

    fix and deploy

---
+++ src/components/tags/Tag.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Tag(props) {
+    return (
+        <div
+            className="bg-blue-100 text-blue-600 px-4 py-1 rounded-full cursor-pointer"
+        >
+            react
+        </div>
+    );
+}
+
+export default Tag;
