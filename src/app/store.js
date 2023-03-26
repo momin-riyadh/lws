92689cfb2e78dd9ae6d6699fc751d8b89c70cc11
momin-riyadh <mominriyadh@gmail.com>
2023-03-26 21:17:45 +0600

    fix and deploy

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,12 @@
 import {configureStore} from '@reduxjs/toolkit';
 import videosReducer from '../features/videos/videosSlice';
+import videoReducer from '../features/video/videoSlice';
 import tagsReducer from "../features/tags/tagsSlice";
 
 export const store = configureStore({
     reducer: {
         videos: videosReducer,
+        video: videoReducer,
         tags: tagsReducer,
     },
 });
--- src/pages/Video.js
+++ src/pages/Video.js
@@ -1,32 +1,54 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import Navbar from "../components/navbar/Navbar";
 import Footer from "../components/Footer";
 import VideoPlayer from "../components/description/Player";
 import VideoDescription from "../components/description/VideoDescription";
 import RelatedVideoList from "../components/list/RelatedVideoList";
+import {useDispatch, useSelector} from "react-redux";
+import {fetchVideo} from "../features/video/videoSlice";
+import {useParams} from "react-router-dom";
+import Loading from "../components/ui/Loading";
 
 function Video(props) {
+    const {video, isLoading, isError,} = useSelector(state => state.video)
+    const dispatch = useDispatch();
+    const {videoId} = useParams();
+
+    useEffect(() => {
+        dispatch(fetchVideo(videoId));
+    }, [dispatch, videoId]);
+
+    const {link, title} = video || {};
+
+    //Decide What to Render
+    let content = null;
+    if (isLoading) content = <Loading/>;
+    if (!isLoading && isError) content = <div className="col-span-12">{error}</div>;
+    if (!isLoading && !isError && !video?.id) {
+        content = <div className="col-span-12">No Video Found!</div>;
+    }
+    if (!isLoading && !isError && video?.id) {
+        content = (
+            <div className="grid grid-cols-3 gap-2 lg:gap-8">
+                <div className="col-span-full w-full space-y-8 lg:col-span-2">
+
+                    <VideoPlayer link={link} title={title}/>
+                    <VideoDescription video={video}/>
+
+                </div>
+
+                <RelatedVideoList/>
+            </div>
+        )
+    }
+
     return (
         <>
-
-
             <section className="pt-6 pb-20">
                 <div className="mx-auto max-w-7xl px-2 pb-20 min-h-[400px]">
-                    <div className="grid grid-cols-3 gap-2 lg:gap-8">
-                        <div className="col-span-full w-full space-y-8 lg:col-span-2">
-
-                            <VideoPlayer/>
-                            <VideoDescription/>
-
-                        </div>
-
-
-                        <RelatedVideoList/>
-                    </div>
+                    {content}
                 </div>
             </section>
-
-
         </>
     );
 }
