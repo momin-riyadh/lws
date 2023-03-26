da0911329f08a3a2a1d40673af0245365d0646a4
momin-riyadh <mominriyadh@gmail.com>
2023-03-26 17:34:05 +0600

    fix and deploy

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,30 @@
+
+C
+server/package.json,5\4\54e8f14248f674b305c14bde70fadbe1da1edb31
+:
+
+.gitignore,a\5\a5cc2925ca8258af241be7e5b0381edf30266302
+E
+server/db_videos.json,e\f\ef4a112671e081729f6b298dad64ab10d951d1e1
+O
+src/components/navbar/Search.js,9\6\96d69ec24797572bafdb3fb6ae5908c7db32648d
+A
+public/index.html,f\4\f43ecdf8d666ee908aacce037b4cd72159e4741b
+K
+src/components/tags/Tags.js,3\1\315de11d9dfb023322956dfa2546230d54d77312
+J
+src/components/tags/Tag.js,0\5\05d09bbce5a0a79da70f00342cb6b9223a98a38b
+O
+src/components/ui/Pagination.js,5\d\5dfce98fb52c8c1eb35628fcc3f1990aab081495
+H
+src/components/Footer.js,b\d\bd22e369de6b4e7c909c8598869bb4c2e3bc1508
+H
+html_template/index.html,4\2\4220ac8c1d851546aa768bb2095c27f7a8825a28
+P
+ src/components/grid/VideoGrid.js,8\d\8d4e8295589761796f0ea5ace7be7b7b777db34b
+<
+src/index.js,f\9\f900edc44cb929c2267a16100a32851f6d5b477d
+X
+(src/components/description/LikeUnlike.js,2\5\251331c028954e08fde9753f21362dfa0ea6b1e3
+^
+.src/components/description/VideoDescription.js,7\d\7da59d275df4f8da277858cb49d4e918c6e5320a
---
+++ src/features/videos/videosSlice.js
@@ -0,0 +1,40 @@
+import {createAsyncThunk, createSlice} from "@reduxjs/toolkit";
+import {getVideos} from "./videosApi";
+
+const initialState = {
+    videos: [],
+    isLoading: false,
+    isError: false,
+    error: "",
+}
+
+//Async Thunk
+export const fetchVideos = createAsyncThunk('videos/fetchVideos',
+    async () => {
+        const videos = await getVideos();
+        return videos;
+    });
+
+const videoSlice = createSlice({
+    name: "videos",
+    initialState,
+    extraReducers: (builder) => {
+        builder
+            .addCase(fetchVideos.pending, (state) => {
+                state.isLoading = true
+                state.isError = false
+            })
+            .addCase(fetchVideos.fulfilled, (state, action) => {
+                state.isLoading = false;
+                state.videos = action.payload;
+            })
+            .addCase(fetchVideos.rejected, (state, action) => {
+                state.isLoading = false;
+                state.videos = [];
+                state.isError = true;
+                state.error = action.error?.message;
+            })
+    }
+});
+
+export default videoSlice.reducer;
